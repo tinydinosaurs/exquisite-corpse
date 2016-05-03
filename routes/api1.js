0cb5cbaf5834222bf508d494e1bb0d43802c5b43
Dana <dana.lachman@gmail.com>
2016-05-03 10:51:24 -0500

    hook up bookshelf api

--- routes/api1.js
+++ routes/api1.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let router = express.Router();
+let path = require('path');
+let bookshelfApi = require('bookshelf-api') ({
+	path: path.join(__dirname, '../', 'models')
+});
 
+
+router.use('/', bookshelfApi);
 // router.get('/products', function(req, res, next) {
 
 // });
