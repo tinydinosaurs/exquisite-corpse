965f2ceaf7d439c6d3bb6b5ddbc15264bfcdc893
Dana <dana.lachman@gmail.com>
2016-05-05 12:45:12 -0500

    filled in home and read components with dummy content to test styles

--- public/scripts/components/pages/Home.js
+++ public/scripts/components/pages/Home.js
@@ -3,11 +3,28 @@ import React from 'react';
 export default React.createClass({
 	render: function() {
 		return (
-			<section>
+			<section className="home">
 				<div className="hero">
 					<h1>...join the story.</h1>
 				</div>
-				<div className="complete-stories">
+				<h1>Read a collaborative story</h1>
+				<div className="complete-stories columns">
+					<div className="story-box column">
+						<img src="/../../../images/indefinite_divisibility_sm.png" />
+						<h2>The fox eats grass</h2>
+					</div>
+					<div className="story-box column">
+						<img src="/../../../images/indefinite_divisibility_sm.png" />
+						<h2>The fox eats grass</h2>
+					</div>
+					<div className="story-box column">
+						<img src="/../../../images/indefinite_divisibility_sm.png" />
+						<h2>The fox eats grass</h2>
+					</div>
+					<div className="story-box column">
+						<img src="/../../../images/indefinite_divisibility_sm.png" />
+						<h2>The fox eats grass</h2>
+					</div>
 				</div>
 			</section>
 			);
