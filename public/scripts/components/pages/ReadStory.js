d29dc996bb82be94d3f504c05127c33e6c6bfdcf
Dana <dana.lachman@gmail.com>
2016-05-11 17:02:14 -0500

    rendered finished story title and content on reads tory page.

--- public/scripts/components/pages/ReadStory.js
+++ public/scripts/components/pages/ReadStory.js
@@ -1,17 +1,76 @@
 import React from 'react';
+import story from '../../models/StoryModel';
+import EntryList from '../subcomponents/EntryList';
+// import entry from '../../collections/EntryCollection';
 
 export default React.createClass({
+	getInitialState: function() {
+		return ({
+			story: new story(
+				{
+					id: this.props.params.storyId
+				}
+			)
+		});
+	},
+
+	componentDidMount: function() {
+		console.log('component mounted.');
+		this.state.story.fetch({
+			data: {
+				withRelated: ['entry']
+			}
+		});
+
+		this.state.story.on('change', this.updateStory);
+	},
+
+	componentWillUnmount: function() {
+		this.state.story.off('change', this.updateStory);
+	},
+
+	updateStory: function() {
+		this.setState({story: this.state.story});
+	},
+
 	render: function() {
+		console.log(this.state.story.get('title'));
+		let entryArray = this.state.story.get('entry');
+		console.log(entryArray);
+
+		if(!entryArray) {
+			entryArray = [];
+			return <div></div>;
+		} 
+			
+		const eachEntry = entryArray.map((val, i, arr) => {
+			console.log(val.content);
+			return (
+				<EntryList 
+					key={val.id}
+					id={val.id}
+					content={val.content}
+				/>
+			);
+		});
+
 		return (
 			<section className="read-story">
-				<h1>The fox eats grass</h1>
-				<p>Surprise pegasus social delight unicorn puppy, pop delight pop rainbows wereunicorn. Wereunicorn pony rainbows kittens social delight delight puppy, rainbows unicorn pegasus delight app pigeon. Kittens puppy puppy delight delight pegasus app puppy app. Kittens delight surprise pop rainbows surprise surprise delight rainbows. Surprise pegasus app pigeon unicorn surprise pony rainbows delight, pony pop social delight delight.</p>
-				<p>Puppy social pegasus delight wereunicorn pop wereunicorn pony wereunicorn. Pop pegasus puppy, wereunicorn app pony pony kittens kittens pegasus. Pegasus rainbows kittens pigeon pony, pigeon rainbows pegasus unicorn surprise social.</p>
-				<p>Pigeon kittens kittens unicorn kittens kittens, app rainbows puppy unicorn. Surprise wereunicorn delight app wereunicorn pigeon, app pegasus pigeon puppy pegasus. Kittens rainbows puppy pop puppy kittens kittens social surprise. Pop pop pigeon delight pegasus pegasus app wereunicorn unicorn. Pony kittens app social unicorn social, social pony pop surprise unicorn surprise delight. Pop rainbows rainbows rainbows, delight app puppy surprise surprise social app surprise. App surprise social rainbows app puppy puppy. Pigeon pony pigeon, delight kittens puppy rainbows app pony pony unicorn. Kittens surprise rainbows delight pigeon pony surprise surprise.</p>
-				<p>Rainbows kittens puppy wereunicorn surprise unicorn, unicorn kittens puppy wereunicorn kittens rainbows surprise. Rainbows app surprise wereunicorn pony, wereunicorn pegasus pegasus delight surprise app. Pony puppy unicorn kittens delight pony pop, unicorn unicorn rainbows social kittens.</p>
-				<p>Pegasus social surprise delight wereunicorn puppy puppy pegasus. Unicorn wereunicorn wereunicorn social delight social pegasus wereunicorn. App pegasus rainbows kittens unicorn unicorn kittens rainbows. Pigeon puppy pony kittens pop puppy pop pony. Pop unicorn rainbows wereunicorn app pop pigeon pop pegasus. Wereunicorn rainbows surprise rainbows kittens kittens surprise delight, rainbows rainbows pigeon unicorn. Pony social rainbows pop delight pigeon app delight kittens. Rainbows surprise unicorn rainbows pony delight app social. Rainbows pegasus pop puppy rainbows pegasus rainbows delight rainbows.</p>
-				<p>Surprise pop surprise app pop, pop delight puppy kittens puppy. Delight app pop, delight pigeon pigeon pony kittens delight rainbows rainbows. Pop pop pony pop wereunicorn app kittens. Pop rainbows kittens rainbows, wereunicorn kittens wereunicorn unicorn rainbows puppy. Unicorn pigeon app delight pop pop rainbows, pop rainbows wereunicorn delight pigeon.</p>
+				<h1>{this.state.story.get('title')}</h1>
+				{eachEntry}
 			</section>
-			);
+		);
 	}
 });
+
+
+
+
+		// if(!entryArray) {
+		// 	entryArray = [];
+		// 	return (
+		// 	<section>{entryArray}</section>
+		// 	);
+		// } else {
+		// 	const eachEntry = entryArray.map();
+		// }
