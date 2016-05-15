9e8ecfec3b37d20f38e3530dc5a285a5961386c0
Dana <dana.lachman@gmail.com>
2016-05-14 19:32:21 -0500

    phase 2 styles in progress.

--- public/scripts/components/subcomponents/StoryThumb.js
+++ public/scripts/components/subcomponents/StoryThumb.js
@@ -4,11 +4,15 @@ import {Link} from 'react-router';
 export default React.createClass({
 	render: function() {
 		return (
-			<div className="story-box column is-half">
-				<img src={this.props.coverImage} />
-				<h2>{this.props.title}</h2>
-				<p>{this.props.content}...</p>
-				<Link to={`/${this.props.urlPath}/${this.props.id}`}>Go to story</Link>
+			<div className="column is-half">
+				<div className="story-box">
+					<img src={this.props.coverImage} />
+					<h2>{this.props.title}</h2>
+					<div className='preview-text'>
+						<p>{this.props.content}...</p>
+					</div>
+					<Link to={`/${this.props.urlPath}/${this.props.id}`}>Go to story</Link>
+				</div>
 			</div>
 		);
 	}
